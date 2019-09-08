ff40cf11d2b2a9b396fc9233da33dc36fb009eca
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-09-08 16:39:14 +0530

    background pattern added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import StudentCard from './components/StudentCard/StudentCard';
 function App() {
   return (
     <div className="App">
+      <h1 className="banner">STUDENT PROFILE EDITOR</h1>
       <StudentCard />
     </div>
   );
